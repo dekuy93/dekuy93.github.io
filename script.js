747491c83652e0cb639c5ee7edb349bc6625684f
dekuy93 <bniu3416@outlook.com>
2023-12-04 16:06:05 +0800

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const train = document.querySelector('.train');
-const speedControl = document.getElementById('speedControl');
-
-speedControl.addEventListener('input', () => {
-  const speed = speedControl.value;
-  train.style.animationDuration = `${11 - speed}s`;
-});
